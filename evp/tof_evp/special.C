0ebc26276f976e1ad4dc79fcfa4f0fb2bec87172
J Schambach <jschamba@physics.utexas.edu>
2006-04-05 16:11:05 +0000

    added root stuff added additional error checks

--- evp/tof_evp/special.C
+++ evp/tof_evp/special.C
@@ -15,6 +15,12 @@
 
 #include <rtsSystems.h>	// for detector ids...
 
+// Root stuff:
+#include "TFile.h"
+#include "TH1.h"
+#include "TTree.h"
+#include "TMapFile.h"
+
 extern int sanityCheck(char *datap) ;
 
 const bool DEBUG = false;
@@ -30,8 +36,18 @@ int main(int argc, char *argv[])
   char *datap ;
   class evpReader *evp = NULL ;
   int good = 0, bad = 0 ;
-  int tofbad1 = 0, tofbad2 = 0, tofbad3 = 0;
+  int 	tofbad1 = 0, // separators missing
+    tofbad2 = 0, // trigger counter out of order
+    tofbad3 = 0, // too many separators 
+    tofbad4 = 0, // DEADFACE wrong 
+    tofbad5 = 0, // trigger ctr diff != event ctr diff 
+    tofbad6 = 0, // trigger ctr in TDC header and trailer out of sync
+    tofbad7 = 0, // token mismatch
+    tofbad8 = 0; // tokens don't match evp token
+  bool trgCtrOutofsync;
+
   static char mountpoint[32] ;
+  static char rootfile[132];
   int fcount ;
   char *fnames[1000] ;	// up to 1000 files, hope that's enough...
   int i ;
@@ -40,24 +56,61 @@ int main(int argc, char *argv[])
   // for TOF:
   u_int expectedMask = 0x3f3;
   int nExpected = 8;
+  int oldEventNo;
+
+  // a Root structure for the events:
+  const int MAX_TRAY = 256;
+  const int MAX_PVPD = 64;
+  struct event_t {
+    Int_t	nTrayLE;
+    Int_t	nTrayTE;
+    Int_t	nEastLE;
+    Int_t	nEastTE;
+    Int_t	nWestLE;
+    Int_t	nWestTE;
+
+    UInt_t	trayLE[MAX_TRAY];
+    UInt_t	trayTE[MAX_TRAY];
+    UInt_t	eastLE[MAX_PVPD];
+    UInt_t	eastTE[MAX_PVPD];
+    UInt_t	westLE[MAX_PVPD];
+    UInt_t	westTE[MAX_PVPD];
+
+    Int_t	trayLEch[MAX_TRAY];
+    Int_t	trayTEch[MAX_TRAY];
+    Int_t	eastLEch[MAX_PVPD];
+    Int_t	eastTEch[MAX_PVPD];
+    Int_t	westLEch[MAX_PVPD];
+    Int_t	westTEch[MAX_PVPD];
+  };
+
+  event_t event;
+
+    
+
   // parse command line arguments
   // but set the defaults before...
 
   int evtype = EVP_TYPE_ANY ;	// any
   strcpy(mountpoint,"") ;
-
+  strcpy(rootfile, "/tmp/out.root");
 
   check_only = 0 ;	// don't _just_ check the headers...
 
   dump_det = -1 ;	// default is no dump
 
-  while((c = getopt(argc,argv,"Ct:d:m:w:D:s:n:")) != EOF)
+  bool doRoot = true;
+
+  while((c = getopt(argc,argv,"CRt:m:r:D:s:n:")) != EOF)
     switch(c) {
     case 't' :
       evtype = atoi(optarg) ;		// request type
       break ;
     case 'm' :				// mountpoint for data on evp i.e. /evp
       strncpy(mountpoint,optarg,sizeof(mountpoint)-1) ;
+      break ;
+    case 'r' :				// mountpoint for data on evp i.e. /evp
+      strncpy(rootfile,optarg,sizeof(rootfile)-1) ;
       break ;
     case 'D' :				// dump data for this detector
       dump_det = atoi(optarg) ;	
@@ -71,9 +124,13 @@ int main(int argc, char *argv[])
     case 'n':
       nExpected = atoi(optarg);
       break;
+    case 'R':
+      doRoot = false;
+      break;
 
     case '?' :
-      fprintf(stdout,"Usage: %s [-t type ] [-C] [-m mountpoint] [-D det_id] [-s separatorMask] [-n nSeparators] <files...>\n",argv[0]) ;
+      fprintf(stdout,"Usage: %s [-t type ] [-C] [-R] [-r rootfile ] [-m mountpoint] [-D det_id] [-s separatorMask] [-n nSeparators] <files...>\n",
+	      argv[0]) ;
       return -1 ;
       break ;
     }
@@ -114,7 +171,7 @@ int main(int argc, char *argv[])
       if(fnames[i]) fprintf(stdout,"Calling constructor [%d/%d], with filename %s\n",(i+1),fcount,fnames[i]) ;
       else fprintf(stdout,"Calling constructor [%d/%d], with filename NULL",(i+1),(fcount+1)) ;
 
-    tofbad1 = tofbad2 = tofbad3 = 0;
+    tofbad1 = tofbad2 = tofbad3 = tofbad4 = tofbad5 = tofbad6 = tofbad7 = tofbad8 = 0;
     evp = new evpReader(fnames[i]) ;
 
     // MUST CHECK THE STATUS!!!
@@ -145,9 +202,41 @@ int main(int argc, char *argv[])
 
     // for TOF checks:
     bool firstTime = true;
+    trgCtrOutofsync = false;
     u_int triggerCtr[10] = {0,0,0,0,0,0,0,0,0,0};
+    u_int triggerWrd[3] = {0,0,0};
     int nSeparators;
       
+
+    TFile hfile;
+    TTree *tree;
+
+    if (doRoot) {
+      // open Root file:
+      //TFile hfile(rootfile, "RECREATE","TOF DAQ ROOT file");
+      hfile.Open(rootfile, "RECREATE","TOF DAQ ROOT file");
+      
+      // create a tree:
+      //TTree *tree = new TTree("Event", "Event");
+      tree = new TTree("Event", "Event");
+      tree->Branch("nEvent", &event.nTrayLE, "nTrayLE/i:nTrayTE:nEastLE:nEastTE:nWestLE:nWestTE");
+      
+      tree->Branch("trayLE", event.trayLE, "trayLE[nTrayLE]/i");
+      tree->Branch("trayTE", event.trayTE, "trayTE[nTrayTE]/i");
+      tree->Branch("eastLE", event.eastLE, "eastLE[nEastLE]/i");
+      tree->Branch("eastTE", event.eastTE, "eastTE[nEastTE]/i");
+      tree->Branch("westLE", event.westLE, "eastLE[nWestLE]/i");
+      tree->Branch("westTE", event.westTE, "eastTE[nWestTE]/i");
+      
+      tree->Branch("trayLEch", event.trayLEch, "trayLEch[nTrayLE]/I");
+      tree->Branch("trayTEch", event.trayTEch, "trayTEch[nTrayTE]/I");
+      tree->Branch("eastLEch", event.eastLEch, "eastLEch[nEastLE]/I");
+      tree->Branch("eastTEch", event.eastTEch, "eastTEch[nEastTE]/I");
+      tree->Branch("westLEch", event.westLEch, "eastLEch[nWestLE]/I");
+      tree->Branch("westTEch", event.westTEch, "eastTEch[nWestTE]/I");
+      
+    }
+
     // The EVENT LOOP!!!!
     for(;;) {
       int end_of_file ;
@@ -197,8 +286,8 @@ int main(int argc, char *argv[])
 	  fprintf(stdout,"Events processed %d...\n",good) ;
 	}
 
-      fprintf(stdout,"\n**** Event %d: bytes %d, token %d, trg_cmd %d, FILE %s\n",
-	      evp->event_number,evp->bytes,evp->token,evp->trgcmd,evp->file_name) ;
+      fprintf(stdout,"\n**** Event %d (seq %d): bytes %d, token %d, trg_cmd %d, FILE %s\n",
+	      evp->event_number,evp->seq, evp->bytes,evp->token,evp->trgcmd,evp->file_name) ;
 
       // make humanly readable time from the UNIX time...
       char *stime ;
@@ -234,43 +323,110 @@ int main(int argc, char *argv[])
 	int tdig, tdc;
 	u_int tdcVal;
 	bool jsErr = false;
+	bool jsErr2 = false;
+	bool tokenErr = false;
+	bool tokenMismatch = false;
 	sep_mask = 0;
 	nSeparators = 0;
+	bool firstTDCHeader = true;
+	bool tdcHeaderError = false;
+	u_int TDCHeader;
+
+	// root structure zero-ing:
+	if(doRoot) {
+	  event.nTrayLE = event.nTrayTE = event.nEastLE = 
+	    event.nEastTE = event.nWestLE = event.nWestTE = 0;
+	}
+
 	for (int ii = 0; ii<4; ii++) { // 4 DDLR banks
 	  halftray = 0;
 	  if (DEBUG) fprintf(stdout,"TOFDDLR %d: length %d words\n",ii+1, tof.ddl_words[ii]);
 	  if (tof.ddl_words[ii] != 0) {
 	    for(int jj=0; jj<tof.ddl_words[ii]; jj++) {
 	      u_int data_word = l2h32(tof.ddl_word_p[ii][jj]); 	// tof.ddl_word_p[ii] points to the DDLR ii data
-	      if ((data_word & 0xF00F0000) == 0xA0040000)	// header trigger word
+	      if (jj == 2) { // this should be deadface
+		if (dump_det == 2) fprintf(stdout, "DDLR %d: 0x%08X\n",ii+1,data_word) ;
+		if (data_word != 0xdeadface) { 
+		  fprintf(stdout, "\t*****JS4:ERROR: 0x%08x should be 0xDEADFACE\n", data_word); 
+		  tofbad4++;
+		}
+	      }
+	      else if ((data_word & 0xFFF00000) == 0xA0000000) {	// header trigger word
 		fprintf(stdout, "DDLR %d: 0x%08X\n",ii+1,data_word) ;
-	      else if ((data_word & 0xF8000000) == 0xE0000000) {	// TDIG separator word
+		triggerWrd[ii] = data_word;
+		if (ii != 0) {
+		  if (data_word != triggerWrd[ii-1]) tokenErr = true;
+		}
+		if ((data_word & 0x00000fff) !=  evp->token) tokenMismatch = true;
+	      }
+	      else if ((data_word & 0xFF000000) == 0xB0000000)	// header DEBUG word
+		fprintf(stdout, "DDLR %d: 0x%08X\n",ii+1,data_word) ;
+	      else if ((data_word & 0xF8FF0000) == 0xE0000000) {	// TDIG separator word
 		nSeparators++;
 		fprintf(stdout, "DDLR %d: 0x%08X\n",ii+1,data_word) ;
-		if (ii == 1) tdig = 9;
-		else if (ii == 2) tdig = 8;
+		if (ii == 1) tdig = 8;
+		else if (ii == 2) tdig = 9;
 		else tdig = (data_word & 0x0F000000) >> 24;
 		sep_mask |= (1<<tdig);
 		data_word &= 0x000000FF;
 		if(DEBUG) fprintf(stdout, "\ttrgCtr new = 0x%x, old = 0x%x\n", data_word, triggerCtr[tdig]);
+#ifdef NOTNOW // don't look for this right now, events might be out of order
 		if (!firstTime) {
 		  if (data_word != ((triggerCtr[tdig]+1)&0xFF)) {
-		    fprintf(stdout, "\t*****JS:ERROR: trgCtr new = 0x%x, old = 0x%x ******\n", 
+		    fprintf(stdout, "\t*****JS2:ERROR: trgCtr new = 0x%x, old = 0x%x ******\n", 
 			    data_word, triggerCtr[tdig]);
 		    jsErr = true;
 		  }
 		}
-		
+#endif
+		//if (evp->event_number == 1) { // the first event analyzed!
+		if (!trgCtrOutofsync) { // counters not yet out of sync
+		  if (tdig != 1) {
+		    if (data_word != triggerCtr[1]) {
+		      fprintf(stdout, "\t*****JSSYNC:ERROR: trgCtr out of sync: 0x%x, TDIG 1 = 0x%x ******\n", 
+			      data_word, triggerCtr[1]);
+		      trgCtrOutofsync = true;
+		    }
+		  }
+		}
+#ifdef NOTNOW	// don't look for this now, since we are running with aborts
+		if (!firstTime) {
+		  if (data_word != ((triggerCtr[tdig]+evp->seq-oldEventNo)&0xFF)) {
+		    fprintf(stdout, "\t*****JS5:ERROR: trgCtr new = 0x%x, old = 0x%x  evnt_diff 0x%x ******\n", 
+			    data_word, triggerCtr[tdig], evp->seq-oldEventNo);
+		    jsErr2 = true;
+		  }
+		}
+#endif
+
 		triggerCtr[tdig] = data_word;
 	      }
-	      else if ((data_word & 0xF0000000) == 0xC0000000) {	// geographical word
+	      else if ((data_word & 0xFFFFFF00) == 0xC0000000) {	// geographical word
 		halftray = data_word & 0x1;
 		if (dump_det == 2) fprintf(stdout, "DDLR %d: 0x%08X\n",ii+1,data_word) ;
 	      }
-	      else if ((data_word & 0x80000000) == 0x80000000) {	// other header words, end-of-event separator
+	      else if (data_word == 0xEA000000) {	// end-of-event separator
 		if (dump_det == 2) fprintf(stdout, "DDLR %d: 0x%08X\n",ii+1,data_word) ;
 	      }
-	      else {	// this should be all the HPTDC words
+	      else if ((data_word & 0x80000000) == 0x80000000) {	
+		// this word has the MSB=1, but fits none of the above defined words, 
+		// and is also not an HPTDC word
+		if (dump_det == 2) fprintf(stdout, "DDLR %d: 0x%08X *** unknown ID ***\n",ii+1,data_word) ;
+	      }
+	      else if ((data_word & 0xE0000000) == 0x20000000) {	// TDC header or trailer
+		fprintf(stdout, "DDLR %d: 0x%08X\n",ii+1,data_word) ;
+		if (firstTDCHeader) {
+		  TDCHeader = data_word & 0x00FFF000;
+		  firstTDCHeader = false;
+		}
+		else {
+		  if ( (data_word & 0x00FFF000) != TDCHeader) { // trigger counter in TDC Header or trailer is wrong
+		    fprintf(stdout, "\t*****JS6:ERROR: trgCtr in TDC header/trailer out of sync ******\n");
+		    tdcHeaderError = true;
+		  }
+		}
+	      }
+	      else {	// this should be all the HPTDC words (anything with the MSB=0)
 		if (dump_det == 2) {
 		  fprintf(stdout, "DDLR %d: 0x%08X", ii+1,data_word) ;
 		  tdc = (data_word & 0x0F000000) >> 24;
@@ -278,11 +434,51 @@ int main(int argc, char *argv[])
 		    channel = ((data_word & 0x00E00000) >> 21) + (tdc&0x3)*8 + (tdc>>2)*24 + halftray*96;
 		    tdcVal =  ((data_word & 0x00180000) >> 19) + ((data_word & 0x0007FFFF) << 2);
 		    fprintf(stdout, " le: ch %3d t 0x%08x", channel, tdcVal);
+		    if (doRoot) {
+		      if (ii == 0) {
+			if (event.nTrayLE < MAX_TRAY) {
+			  event.trayLE[event.nTrayLE] = tdcVal;
+			  event.trayLEch[event.nTrayLE++] = channel;
+			}
+		      }
+		      else if (ii == 2) { // West
+			if (event.nWestLE < MAX_PVPD) {
+			  event.westLE[event.nWestLE] = tdcVal;
+			  event.westLEch[event.nWestLE++] = channel;
+			}
+		      }
+		      else if (ii == 1) { // East
+			if (event.nEastLE < MAX_PVPD) {
+			  event.eastLE[event.nEastLE] = tdcVal;
+			  event.eastLEch[event.nEastLE++] = channel;
+			}
+		      }
+		    } // if doRoot
 		  }
 		  else if ((data_word & 0xF0000000) == 0x50000000) {	// trailing edge, 100ps
 		    channel = ((data_word & 0x00F80000) >> 19) + (tdc>>2)*24 + halftray*96;
 		    tdcVal =  (data_word & 0x0007FFFF)<<2;
 		    fprintf(stdout, " te: ch %3d t 0x%08x", channel, tdcVal);
+		    if (doRoot) {
+		      if (ii == 0) {
+			if (event.nTrayTE < MAX_TRAY) {
+			  event.trayTE[event.nTrayTE] = tdcVal;
+			  event.trayTEch[event.nTrayTE++] = channel;
+			}
+		      }
+		      else if (ii == 2) { // West
+			if (event.nWestLE < MAX_PVPD) {
+			  event.westTE[event.nWestTE] = tdcVal;
+			  event.westTEch[event.nWestTE++] = channel;
+			}
+		      }
+		      else if (ii == 1) { // East
+			if (event.nEastLE < MAX_PVPD) {
+			  event.eastTE[event.nEastTE] = tdcVal;
+			  event.eastTEch[event.nEastTE++] = channel;
+			}
+		      }
+		    } // if doRoot
 		  }
 		  fprintf(stdout, "\n");
 		}
@@ -291,23 +487,51 @@ int main(int argc, char *argv[])
 	  } // if (tof.ddl_words[ii] != 0)
 	} // for (int ii = ....
 
+
+	if (tokenErr) {
+	  fprintf(stdout, "\t***JS7:ERROR: tokens don't match: 0x%x, 0x%x, 0x%x\n",
+		  triggerWrd[0], triggerWrd[1], triggerWrd[2]);
+	  tofbad7++;
+	}
+	if (tokenMismatch) {
+	  fprintf(stdout, "\t***JS8:ERROR: token(s) don't match event token: evp 0x%x, TOF 0x%x, 0x%x, 0x%x\n",
+		  evp->token, triggerWrd[0], triggerWrd[1], triggerWrd[2]);
+	  tofbad8++;
+	}
+	oldEventNo =  evp->seq;
 	if (firstTime) firstTime = false;
+	if (tdcHeaderError) tofbad6++;
 	if (sep_mask != expectedMask) {
-	  fprintf(stdout, "\t*****JS:ERROR: Separator(s) missing (0x%03x); size DDLR 1: %d, 3: %d\n", 
+	  fprintf(stdout, "\t*****JS1:ERROR: Separator(s) missing (0x%03x); size DDLR 1: %d, 3: %d\n", 
 		  sep_mask, tof.ddl_words[0], tof.ddl_words[2]);
 	  firstTime = true;
 	  tofbad1++;
+	  continue;
 	}
 	if (jsErr) {
 	  firstTime = true;
 	  tofbad2++;
+	  continue;
+	}
+	if (jsErr2) {
+	  firstTime = true;
+	  tofbad5++;
+	  continue;
 	}
 	if (nSeparators > nExpected) {
-	  fprintf(stdout, "\t***JS:ERROR: number of separators: %d\n", nSeparators);
+	  fprintf(stdout, "\t***JS3:ERROR: number of separators: %d\n", nSeparators);
 	  tofbad3++;
 	  firstTime = true;
+	  continue;
 	}
+	// print out the structure for tray leading edge:
+	//printf("nTrayLE %d nTrayTE %d\n", event.nTrayLE, event.nTrayTE);
+	//for (int kk = 0; kk<event.nTrayLE; kk++)
+	//  printf("ch %d tdc 0x%x\n", event.trayLEch[kk], event.trayLE[kk]);
 
+	if (doRoot) {
+	  tree->Fill();
+	}
 	
       } // else
       
@@ -315,10 +539,18 @@ int main(int argc, char *argv[])
 
 
     fprintf(stdout,"Processed %s: %d good, %d bad events total...\n",fnames[i],good,bad) ;
-    fprintf(stdout,"%d bad1, %d bad2, %d bad3 events with TOF problems\n", 
-	    tofbad1, tofbad2, tofbad3); 
+    fprintf(stdout,"%d bad1, %d bad2, %d bad3 %d bad4 %d bad5 %d bad6 %d bad7 %d bad8 events with TOF problems\n", 
+	    tofbad1, tofbad2, tofbad3, tofbad4, tofbad5, tofbad6, tofbad7, tofbad8); 
+    if (trgCtrOutofsync) fprintf(stdout, "Trigger Counters in separators out of sync\n");
 
     delete evp ;
+    
+    // close Root file:
+    if (doRoot) {
+      hfile.Write();
+      hfile.Close();
+    }
+
   }	// end of input file loop
 
   return 0 ;
