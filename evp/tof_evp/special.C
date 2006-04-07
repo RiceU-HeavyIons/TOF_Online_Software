feae343fc6b94590a206aeebd1ceaf8446ecf338
J Schambach <jschamba@physics.utexas.edu>
2006-04-07 20:23:19 +0000

    Several bug fixes in the Root Tree filling code Added INL corrected values to Root Tree Added INL correction code

--- evp/tof_evp/special.C
+++ evp/tof_evp/special.C
@@ -10,6 +10,9 @@
 #include <string.h>
 #include <time.h>
 
+// C++ headers
+#include <iostream>
+using namespace std;
 
 #include <evpReader.hh>
 
@@ -35,7 +38,7 @@ int main(int argc, char *argv[])
   int c ;
   char *datap ;
   class evpReader *evp = NULL ;
-  int good = 0, bad = 0 ;
+  int good = 0, bad = 0, nRootEv = 0;
   int 	tofbad1 = 0, // separators missing
     tofbad2 = 0, // trigger counter out of order
     tofbad3 = 0, // too many separators 
@@ -82,6 +85,13 @@ int main(int argc, char *argv[])
     Int_t	eastTEch[MAX_PVPD];
     Int_t	westLEch[MAX_PVPD];
     Int_t	westTEch[MAX_PVPD];
+
+    Float_t	trayLEc[MAX_TRAY];
+    Float_t	trayTEc[MAX_TRAY];
+    Float_t	eastLEc[MAX_PVPD];
+    Float_t	eastTEc[MAX_PVPD];
+    Float_t	westLEc[MAX_PVPD];
+    Float_t	westTEc[MAX_PVPD];
   };
 
   event_t event;
@@ -109,7 +119,7 @@ int main(int argc, char *argv[])
     case 'm' :				// mountpoint for data on evp i.e. /evp
       strncpy(mountpoint,optarg,sizeof(mountpoint)-1) ;
       break ;
-    case 'r' :				// mountpoint for data on evp i.e. /evp
+    case 'r' :				// Root file to save Root tree in
       strncpy(rootfile,optarg,sizeof(rootfile)-1) ;
       break ;
     case 'D' :				// dump data for this detector
@@ -129,8 +139,9 @@ int main(int argc, char *argv[])
       break;
 
     case '?' :
-      fprintf(stdout,"Usage: %s [-t type ] [-C] [-R] [-r rootfile ] [-m mountpoint] [-D det_id] [-s separatorMask] [-n nSeparators] <files...>\n",
+      fprintf(stdout,"Usage: %s [-t type ] [-C] [-R] [-r rootfile ] [-m mountpoint]",
 	      argv[0]) ;
+      fprintf(stdout," [-D det_id] [-s separatorMask] [-n nSeparators] <files...>\n");
       return -1 ;
       break ;
     }
@@ -151,7 +162,7 @@ int main(int argc, char *argv[])
   }
 
 
-  good = bad = 0 ;
+  good = bad = nRootEv = 0 ;
   
   for(i=0;i<fcount;i++) {
     // One must first call the constructor of the data
@@ -208,13 +219,17 @@ int main(int argc, char *argv[])
     int nSeparators;
       
 
-    TFile hfile;
+    TFile *hfile;
     TTree *tree;
 
     if (doRoot) {
+      int ReadINLtable(void);
+      // Read the INL tables
+      if (ReadINLtable() != 0) return -1;
+      
       // open Root file:
       //TFile hfile(rootfile, "RECREATE","TOF DAQ ROOT file");
-      hfile.Open(rootfile, "RECREATE","TOF DAQ ROOT file");
+      hfile = new TFile(rootfile, "RECREATE","TOF DAQ ROOT file");
       
       // create a tree:
       //TTree *tree = new TTree("Event", "Event");
@@ -225,16 +240,22 @@ int main(int argc, char *argv[])
       tree->Branch("trayTE", event.trayTE, "trayTE[nTrayTE]/i");
       tree->Branch("eastLE", event.eastLE, "eastLE[nEastLE]/i");
       tree->Branch("eastTE", event.eastTE, "eastTE[nEastTE]/i");
-      tree->Branch("westLE", event.westLE, "eastLE[nWestLE]/i");
-      tree->Branch("westTE", event.westTE, "eastTE[nWestTE]/i");
+      tree->Branch("westLE", event.westLE, "westtLE[nWestLE]/i");
+      tree->Branch("westTE", event.westTE, "westTE[nWestTE]/i");
       
       tree->Branch("trayLEch", event.trayLEch, "trayLEch[nTrayLE]/I");
       tree->Branch("trayTEch", event.trayTEch, "trayTEch[nTrayTE]/I");
       tree->Branch("eastLEch", event.eastLEch, "eastLEch[nEastLE]/I");
       tree->Branch("eastTEch", event.eastTEch, "eastTEch[nEastTE]/I");
-      tree->Branch("westLEch", event.westLEch, "eastLEch[nWestLE]/I");
-      tree->Branch("westTEch", event.westTEch, "eastTEch[nWestTE]/I");
+      tree->Branch("westLEch", event.westLEch, "westLEch[nWestLE]/I");
+      tree->Branch("westTEch", event.westTEch, "westTEch[nWestTE]/I");
       
+      tree->Branch("trayLEc", event.trayLEc, "trayLEc[nTrayLE]/F");
+      tree->Branch("trayTEc", event.trayTEc, "trayTEc[nTrayTE]/F");
+      tree->Branch("eastLEc", event.eastLEc, "eastLEc[nEastLE]/F");
+      tree->Branch("eastTEc", event.eastTEc, "eastTEc[nEastTE]/F");
+      tree->Branch("westLEc", event.westLEc, "westLEc[nWestLE]/F");
+      tree->Branch("westTEc", event.westTEc, "westTEc[nWestTE]/F");
     }
 
     // The EVENT LOOP!!!!
@@ -435,21 +456,25 @@ int main(int argc, char *argv[])
 		    tdcVal =  ((data_word & 0x00180000) >> 19) + ((data_word & 0x0007FFFF) << 2);
 		    fprintf(stdout, " le: ch %3d t 0x%08x", channel, tdcVal);
 		    if (doRoot) {
+		      float GetINLcorr(int,int,int);
 		      if (ii == 0) {
 			if (event.nTrayLE < MAX_TRAY) {
 			  event.trayLE[event.nTrayLE] = tdcVal;
+			  event.trayLEc[event.nTrayLE] = (Float_t)tdcVal + GetINLcorr(4,channel,(tdcVal&0x3ff));
 			  event.trayLEch[event.nTrayLE++] = channel;
 			}
 		      }
 		      else if (ii == 2) { // West
 			if (event.nWestLE < MAX_PVPD) {
 			  event.westLE[event.nWestLE] = tdcVal;
+			  event.westLEc[event.nWestLE] = (Float_t)tdcVal + GetINLcorr(4,channel+216,(tdcVal&0x3ff));
 			  event.westLEch[event.nWestLE++] = channel;
 			}
 		      }
 		      else if (ii == 1) { // East
 			if (event.nEastLE < MAX_PVPD) {
 			  event.eastLE[event.nEastLE] = tdcVal;
+			  event.eastLEc[event.nEastLE] = (Float_t)tdcVal + GetINLcorr(4,channel+192,(tdcVal&0x3ff));
 			  event.eastLEch[event.nEastLE++] = channel;
 			}
 		      }
@@ -457,24 +482,31 @@ int main(int argc, char *argv[])
 		  }
 		  else if ((data_word & 0xF0000000) == 0x50000000) {	// trailing edge, 100ps
 		    channel = ((data_word & 0x00F80000) >> 19) + (tdc>>2)*24 + halftray*96;
-		    tdcVal =  (data_word & 0x0007FFFF)<<2;
+		    tdcVal =  (data_word & 0x0007FFFF)<<2; // convert to 25ps bins
 		    fprintf(stdout, " te: ch %3d t 0x%08x", channel, tdcVal);
 		    if (doRoot) {
+		      float GetINLcorr(int,int,int);
 		      if (ii == 0) {
 			if (event.nTrayTE < MAX_TRAY) {
 			  event.trayTE[event.nTrayTE] = tdcVal;
+			  event.trayTEc[event.nTrayTE] = (Float_t)tdcVal + 
+			    4. * GetINLcorr(4,channel,(data_word&0xff));
 			  event.trayTEch[event.nTrayTE++] = channel;
 			}
 		      }
 		      else if (ii == 2) { // West
-			if (event.nWestLE < MAX_PVPD) {
+			if (event.nWestTE < MAX_PVPD) {
 			  event.westTE[event.nWestTE] = tdcVal;
+			  event.westTEc[event.nWestTE] = (Float_t)tdcVal + 
+			    4. * GetINLcorr(4,channel+216,(data_word&0xff));
 			  event.westTEch[event.nWestTE++] = channel;
 			}
 		      }
 		      else if (ii == 1) { // East
-			if (event.nEastLE < MAX_PVPD) {
+			if (event.nEastTE < MAX_PVPD) {
 			  event.eastTE[event.nEastTE] = tdcVal;
+			  event.eastTEc[event.nEastTE] = (Float_t)tdcVal + 
+			    4. * GetINLcorr(4,channel+192,(data_word&0xff));
 			  event.eastTEch[event.nEastTE++] = channel;
 			}
 		      }
@@ -502,8 +534,8 @@ int main(int argc, char *argv[])
 	if (firstTime) firstTime = false;
 	if (tdcHeaderError) tofbad6++;
 	if (sep_mask != expectedMask) {
-	  fprintf(stdout, "\t*****JS1:ERROR: Separator(s) missing (0x%03x); size DDLR 1: %d, 3: %d\n", 
-		  sep_mask, tof.ddl_words[0], tof.ddl_words[2]);
+	  fprintf(stdout, "\t*****JS1:ERROR: Separator(s) missing (0x%03x, expected 0x%03x); size DDLR 1: %d, 3: %d\n", 
+		  sep_mask, expectedMask, tof.ddl_words[0], tof.ddl_words[2]);
 	  firstTime = true;
 	  tofbad1++;
 	  continue;
@@ -531,6 +563,8 @@ int main(int argc, char *argv[])
 
 	if (doRoot) {
 	  tree->Fill();
+	  nRootEv++;
+	  // fprintf(stdout, "\t***JSROOT: saved %d good %d\n", nRootEv, good);
 	}
 	
       } // else
@@ -543,15 +577,97 @@ int main(int argc, char *argv[])
 	    tofbad1, tofbad2, tofbad3, tofbad4, tofbad5, tofbad6, tofbad7, tofbad8); 
     if (trgCtrOutofsync) fprintf(stdout, "Trigger Counters in separators out of sync\n");
 
+    fprintf(stderr,"Processed %s: %d good, %d bad events total, %d events in Root ...\n",fnames[i],good,bad,nRootEv) ;
+    fprintf(stderr,"%d bad1, %d bad2, %d bad3 %d bad4 %d bad5 %d bad6 %d bad7 %d bad8 events with TOF problems\n", 
+	    tofbad1, tofbad2, tofbad3, tofbad4, tofbad5, tofbad6, tofbad7, tofbad8); 
+    if (trgCtrOutofsync) fprintf(stderr, "Trigger Counters in separators out of sync\n");
+
     delete evp ;
     
     // close Root file:
     if (doRoot) {
-      hfile.Write();
-      hfile.Close();
+      fprintf(stderr, "Closing Root File...\n");
+      hfile->Write();
+      hfile->Close();
     }
 
   }	// end of input file loop
 
   return 0 ;
+}
+
+float INLtable[10][4][1024];
+
+int ReadINLtable(void)
+{
+  cout << " Reading INL tables" << endl;
+  string boardid[10]={"mx38162","mx36804","mx36803","mx36805","mx36802",
+		      "mx38163","mx36799","mx36801","mx38164","mx38161"};
+  
+  string filetdcid[4]={"1","2","3","4"};
+  string filehitid[4]={"1","10","22","1"};
+  string topdir="/homes/jschamba/tof/inltabledat/";
+  char tmpstr[400];
+  for(int iboard=0; iboard<10; iboard++){
+    for(int itdc=0; itdc<4; itdc++){
+      for(int i=0; i<1024; i++) {INLtable[iboard][itdc][i] = 0.;}
+      //cout<<"Process board "<<boardid[iboard]<<endl;
+      if(boardid[iboard] == "mx38163") boardid[iboard] = "mx38162";
+      sprintf(tmpstr, "%s/%s-tdc%s-h%s.inl-1024", 
+	      topdir.c_str(), boardid[iboard].c_str(),
+              filetdcid[itdc].c_str(), filehitid[itdc].c_str());
+      if(itdc==3)sprintf(tmpstr, "%s/%s-tdc%s-h%s.inl-256", 
+			 topdir.c_str(), boardid[iboard].c_str(),
+			 filetdcid[itdc].c_str(), filehitid[itdc].c_str());
+      
+      FILE* inputfile=fopen(tmpstr,"r");
+      if(!inputfile) {
+      	cout<<"Can not open "<<tmpstr<<" Please check!!!"<<endl;
+        return -1;
+      }
+      
+      float bin;
+      float data;
+      while ( (fscanf(inputfile, "%f%f", &bin, &data)) != EOF) {
+	int ibin = int(bin - 0.5);
+	INLtable[iboard][itdc][ibin] = float(int(data*1000))/1000.;
+      }
+      fclose(inputfile); 
+      cout << "INL table ====================>" << endl;
+      cout << "board id =" << boardid[iboard] << " TDCid=" << itdc << endl;
+      for(int i=0; i<10; i++) {cout << i << "->" << INLtable[iboard][itdc][i] << " ";}
+      cout << endl;
+      for(int i=1014; i<1024; i++) {cout << i << "->" << INLtable[iboard][itdc][i] << " ";}
+      cout << endl;
+    }  	// end of loop TDC
+  } 	// end of loop board
+  return 0;
+}
+
+
+/*
+ * The INL correction returned from this function is to be 
+ * added to the TDC value
+ * 
+ * edgeflag is "4" for leading and "5" for trailing edge
+ * tdcchan is 0-191 for tray, 192-215 for pVPD east,
+ * 216-239 for pVPD west
+ * bin is 0-1023 for leading, 0 - 255 for trailing edge
+ */
+
+float GetINLcorr(int edgeflag,int tdcchan,int bin)
+{
+  int iboard = tdcchan / 24;
+  int chan = tdcchan % 24;
+  int itdc = 0;
+  if(edgeflag==4) itdc = chan/8;
+  if(edgeflag==5) itdc = 3;
+  //if(tdcchan==191&&edgeflag==4) itdc=0;
+  //if(tdcchan==192&&edgeflag==4) itdc=1;
+  //if(tdcchan==193&&edgeflag==4) itdc=1;
+  //if(tdcchan==194&&edgeflag==4) itdc=0;
+  //if(tdcchan==195&&edgeflag==4) itdc=1;
+  //if(tdcchan==196&&edgeflag==4) itdc=1;
+
+  return INLtable[iboard][itdc][bin];
 }
