828f6845a139b411b6de65ae923b3358fad075e5
J Schambach <jschamba@physics.utexas.edu>
2007-05-14 15:04:27 +0000

    updated for new HLP protocol for TDIG revisions  D and E

--- pcan/setThreshold.cc
+++ pcan/setThreshold.cc
@@ -7,10 +7,11 @@
 
 #ifndef lint
 static char  __attribute__ ((unused)) vcid[] = 
-"$Id: setThreshold.cc,v 1.2 2005-01-24 21:47:53 jschamba Exp $";
+"$Id: setThreshold.cc,v 1.3 2007-05-14 15:04:27 jschamba Exp $";
 #endif /* lint */
 
 #define LOCAL_DEBUG
+#define TDIG_D
 
 //****************************************************************************
 // INCLUDES
@@ -36,6 +37,8 @@ int main(int argc, char *argv[])
 {
   unsigned int nodeID;
   double tVal;
+  string cmdString;
+  stringstream ss;
   //char cmdString[255];
 
   if (debug) cout << vcid << endl;
@@ -46,14 +49,63 @@ int main(int argc, char *argv[])
     return 1;
   }
   
-  nodeID = atoi(argv[1]);
+  nodeID = strtol(argv[1], (char **)NULL, 0);
+
+  tVal = atof(argv[2]);
+
+#ifdef TDIG_D
+  if ((tVal < 0) || (tVal > 3300)) {
+    printf("ERROR: Input voltage %d is out of range.  Voltage must be between 0 mV and 3300 mV.\n",
+	   tVal);
+    return 1;
+  }
+
+  /* TDIG-D uses a different DAC (TI DAC7571)
+   * Ideal conversion per datasheet page 12 is
+   *       Vout = Vdd * (DAC/4095)
+   * where Vout is resulting output voltage
+   *       Vdd is nominal supply voltage (3.3V)
+   *       DAC is DAC setting
+   * We are supplied here with desired voltage (millivolts) and need to determine D.
+   * Rearranging: DAC = (integer) Vout/Vdd * 4095
+   * Vdd = 3300 mV,
+   */
+  double D = (tVal * (4095.0 / 3300.0)) + 0.5; // 0.5 for rounding
+  int decVal = (int)D;
+  decVal &= 0xFFF;   // mask to 12 bits, bits PD0, PD1 are always zero (not pwr'd down)
+  
+  unsigned short ID = 0x002 | (nodeID << 4);  // Write Message
+  unsigned short DATA0 = 0x8;
+  unsigned short DATA1 = decVal & 0x00FF;
+  unsigned short DATA2 = (decVal>>8) & 0x000F;
+
+  if (debug)
+    cout << "nodeID = " << nodeID
+	 << ", thresholdVal = " << tVal
+	 << "mV;  msgID = " << showbase << hex << ID
+	 << " DATA[0] = " << DATA0
+	 << " DATA[1] = " << DATA1
+	 << " DATA[2] = " << DATA2 << endl;
+
+
+  ss << "./pc \"m s " << showbase << hex << ID
+     << " 3 " << DATA0
+     << " " << DATA1
+     << " " << DATA2
+     << "\"";
+
+#else // Run 5 TDIG
   if ((nodeID < 0) || (nodeID > 7)) { 
     cerr << "nodeID = " << nodeID 
 	 << " is an invalid entry.  Use a value between 0 and 7 instead.\n";
     return -1;
   }
 
-  tVal = atof(argv[2]);
+  if ((tVal < -100) || (tVal > 100)) {
+    printf("ERROR: Input voltage %d is out of range.  Voltage must be between -100 mV and 100 mV.\n",
+	   tVal);
+    return 1;
+  }
 
   double D = -20.003*tVal + 2030.5; // the result of the equation
   int decVal = int(D); // the decimal value D gets truncated to
@@ -72,11 +124,12 @@ int main(int argc, char *argv[])
 
   //sprintf(cmdString, "./pc \"m s 0x%x 2 0x%x 0x%x\"", ID, DATA0, DATA1);
 
-  string cmdString;
-  stringstream ss;
   ss << "./pc \"m s " << showbase << hex << ID
      << " 2 " << DATA0
      << " " << DATA1 << "\"";
+#endif
+
+  // now send the message:
   cmdString = ss.str();
 
   if (debug) cout << "cmdString = " << cmdString << endl;
