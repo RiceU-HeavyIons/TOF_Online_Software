05be1505f3ee265b720dc2f85644a346b4371882
J Schambach <jschamba@physics.utexas.edu>
2009-11-06 15:29:35 +0000

    change file mode of log files to 00666

--- anaconda_two/src/AnLog.cpp
+++ anaconda_two/src/AnLog.cpp
@@ -5,23 +5,29 @@
  *      Author: koheik
  */
 #include <QtCore/QDateTime>
+#include <sys/types.h>
+#include <sys/stat.h>
 #include "AnLog.h"
 
 AnLog::AnLog(QString path)
 {
-	m_fp = fopen(path.toStdString().c_str(), "a+");
+  int cm;
+  m_fp = fopen(path.toStdString().c_str(), "a+");
+  if (m_fp == (FILE *)NULL) perror("Log File");
+  else
+    cm = chmod(path.toStdString().c_str(), 00666);
 }
 
 AnLog::~AnLog()
 {
-	fclose(m_fp);
+  fclose(m_fp);
 }
 
 void AnLog::log(QString str)
 {
-	QDateTime now = QDateTime::currentDateTime();
-	fprintf(m_fp, "[%s] ", now.toString("yyyy-MM-dd hh:mm:ss").toStdString().c_str());
-	fprintf(m_fp, "%s", str.toStdString().c_str());
-	fprintf(m_fp, "\n");
-	fflush(m_fp);
+  QDateTime now = QDateTime::currentDateTime();
+  fprintf(m_fp, "[%s] ", now.toString("yyyy-MM-dd hh:mm:ss").toStdString().c_str());
+  fprintf(m_fp, "%s", str.toStdString().c_str());
+  fprintf(m_fp, "\n");
+  fflush(m_fp);
 }
