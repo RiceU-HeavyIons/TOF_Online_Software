a9d5fcd9a039fe579564f540846d8d1c9a933b4d
J Schambach <jschamba@physics.utexas.edu>
2009-12-17 15:19:23 +0000

    changed threshold to 2500

--- pcan/turnonscriptMTD.sh
+++ pcan/turnonscriptMTD.sh
@@ -8,8 +8,8 @@ usleep 500000
 usleep 1300000
 
 
-# set threshold on each TDIG board to 1200mV
-./xsetThreshold 255 0x7f 0x20 1200
+# set threshold on each TDIG board to 2500mV
+./xsetThreshold 255 0x7f 0x20 2500
 usleep 1500000
 # put TCPUs into run mode and turn on Serdes, turn off CANbus data
 ./pc "m s 0x202 3 0xe 0x2 0xf 255"
