9780dad3375e7a80303ff2e5e1af43e87f46c711
J Schambach <jschamba@physics.utexas.edu>
2010-03-20 19:26:55 +0000

    changed threshold back to 1200mV for 62GeV running

--- pcan/turnonscriptNE.sh
+++ pcan/turnonscriptNE.sh
@@ -43,8 +43,8 @@ done
 #    usleep 70000
 #done
 
-# set threshold on each TDIG board to 2500mV
-./xsetThreshold 254 0x7f 0x7f 2500
+# set threshold on each TDIG board to 1200mV
+./xsetThreshold 254 0x7f 0x7f 1200
 usleep 1500000
 # put TCPUs into run mode and turn on Serdes, turn off CANbus data
 ./pc "m s 0x7f2 3 0xe 0x2 0xf 254"
