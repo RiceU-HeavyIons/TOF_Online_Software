745f61cd48e76eb886bad0b503434ff50657cae4
J Schambach <jschamba@physics.utexas.edu>
2012-01-24 18:54:07 +0000

    web entry form for MTD runs (for now only noise runs)

---
+++ UtWeb/www/mtd_tray/create_noiserun.php
@@ -0,0 +1,65 @@
+<?php
+include("../../lib/connect.php");
+include("../../lib/orm.php");
+include("../../lib/session.php");
+Session::check_login("../mtd_tray/create_noiserun.php");
+
+function back(){
+  echo "<form action='list.php' method='post'>";
+  echo "<input name='back' type='submit' value='Back'/>";
+  echo "</form>";
+}
+
+include("../../lib/template.php");
+tsw_header("MTD Noise Run Creation Page");
+tsw_toolbar();
+?>
+
+<div id="main">
+<div class='title'>MTD Noise Run</div>
+
+<?php
+  echo "<table><tr><td>", back(),"</td><td>";
+  echo "<form name='new_noiserun' action='./new_run.php' method='post'/>";
+  echo "<input name='request_type' type='hidden' value='new_run'/>";
+
+  echo "<tr><td>Tray Serial:<br>";
+  echo "<input name='tray_sn' type='text' size='8'/></td></tr>";
+
+  echo "<tr><td>Select User<br>";
+  echo "<select name='user'>";
+  $result_users=mysql_query("select * from users where email!=''", $con);
+  while ($users=mysql_fetch_object($result_users)){
+    echo "<option>", $users->name, "</option>";
+  }
+  echo "<td>Select Status:<br>";
+  echo "<select name='run_status'>";
+  $result_status=mysql_query("select * from run_status", $con);
+  while ($r_status=mysql_fetch_object($result_status)){
+    echo "<option>", $r_status->name, "</option>";
+  }
+  echo "</td><td>Run Number:<br>";
+  echo "<input name='run_no' type='text' size='11'/></td></tr>";
+
+  echo "<tr><td>Run Start:<br>";
+  echo "<input name='start_date' type='text' value='YYYY-MM-DD HH:MM:SS' size='21'/></td>";
+  echo "<td>Run End:<br>";
+  echo "<input name='end_date' type='text' value='YYYY-MM-DD HH:MM:SS' size='21'/></td>";
+  echo "<td>Events:<br>";
+  echo "<input name='event_no' type='text' size='11'/></td></tr>";
+
+  echo "<tr><td colspan='5'>Data Location:<br>";
+  echo "<input name='data_uri' type='text' size='75'/></td></tr>";
+
+  echo "<tr><td colspan='5'>Results PDF Location:<br>";
+  echo "<input name='results_uri' type='text' size='75'/></td></tr>";
+
+  echo "<tr><td colspan='4'>Notes:<br>";
+  echo "<textarea name='text' rows='5' cols='35' type='text'/></textarea><br>";
+
+  echo "<input name='new_run' type='submit' value='Insert this information into database'/>";
+  echo "</form></td></tr></table>";
+?>
+
+<?php tsw_footer(); ?>
+<?php mysql_close($con) ?>
---
+++ UtWeb/www/mtd_tray/new_run.php
@@ -0,0 +1,50 @@
+<?php
+include("../../lib/connect.php");
+include("../../lib/session.php");
+
+$type         = $_POST['request_type'];
+$tray_sn      = $_POST['tray_sn'];
+$user         = $_POST['user'];
+$run_status   = $_POST['run_status'];
+$run_no       = $_POST['run_no'];
+$event_no     = $_POST['event_no'];
+$start_date   = $_POST['start_date'];
+$end_date     = $_POST['end_date'];
+$data_uri     = $_POST['data_uri'];
+$results_uri  = $_POST['results_uri'];
+$notes        = $_POST['text'];
+
+Session::check_login("../mtd_tray/new_run.php");
+
+if ($type == 'new_run') {
+  if($start_date=='' || $start_date=='YYYY-MM-DD HH:MM:SS'){
+    $start_date=date(" Y-m-d H:i:s");
+  }
+  if($end_date=='' || $end_date=='YYYY-MM-DD HH:MM:SS'){
+    $end_date=date(" Y-m-d H:i:s");
+  }
+  $result1 = mysql_query("select id from trays where sn= '$tray_sn' ", $con);
+  $r1 = mysql_fetch_object($result1);
+  $tray_id = $r1->id;
+
+  $result2 = mysql_query("select id from users where name= '$user' ", $con);
+  $r2 = mysql_fetch_object($result2);
+  $user_id = $r2->id;
+
+  $result3 = mysql_query("select id from run_status where name= '$run_status' ", $con);
+  $r3 = mysql_fetch_object($result3);
+  $run_status_id = $r3->id;
+
+
+  $sql = "INSERT INTO mtd_runs (id, tray_id, run_status_id, run_type_id, user_id, start, stop, ";
+  $sql .= "run_time, events, data_uri, results_uri, note) VALUES (";
+  $sql .= "'$run_no', '$tray_id', '$run_status_id', 4, '$user_id', '$start_date', '$end_date', ";
+  $sql .= "default, '$event_no', '$data_uri', '$results_uri', '$notes' )";
+
+  $insert = mysql_query($sql, $con) or die(mysql_error()); 
+  header( "Location: ./create_noiserun.php");
+}
+
+
+
+?>
