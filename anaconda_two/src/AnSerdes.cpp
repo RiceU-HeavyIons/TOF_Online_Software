5de8e3c748c2d1d21d179c41a7022d8fbc3228ae
J Schambach <jschamba@physics.utexas.edu>
2012-04-04 17:49:00 +0000

    let "relink" command proceed if less than 3 communication errors

--- anaconda_two/src/AnSerdes.cpp
+++ anaconda_two/src/AnSerdes.cpp
@@ -10,10 +10,10 @@
 #include "AnExceptions.h"
 
 AnSerdes::AnSerdes(const AnAddress& laddr, const AnAddress& haddr, AnCanObject *parent)
- : AnBoard(laddr, haddr, parent)
+  : AnBoard(laddr, haddr, parent)
 {
-	setObjectName(QString("SERDES ") + lAddress().toString());
-	for(int i = 1; i <= NPORT; ++i) setTcpu(i, NULL);
+  setObjectName(QString("SERDES ") + lAddress().toString());
+  for(int i = 1; i <= NPORT; ++i) setTcpu(i, NULL);
 }
 
 AnSerdes::~AnSerdes()
@@ -29,25 +29,25 @@ QString AnSerdes::firmwareString() const
 
 QString AnSerdes::dump() const
 {
-	QStringList sl;
+  QStringList sl;
 
-	sl << QString().sprintf("AnSerdes(%p):", this);
-	sl << QString("  Name              : ") + name();
-	sl << QString("  Hardware Address  : ") + haddr().toString().toStdString().c_str();
-	sl << QString("  Logical Address   : ") + laddr().toString().toStdString().c_str();
-	sl << QString("  Installed         : ") + (installed() ? "yes" : "no");
-	sl << QString("  Active            : ") + (active() ? "yes" : "no");
-	sl << QString("  Synchronized      : ") + synced().toString();
-	sl << QString("  Firmware ID       : ") + firmwareString();
-	sl << QString("  ECSR              : 0x") + QString::number(ecsr(), 16);
-	sl << QString("  PLD REG9x Set     : 0x") + QString::number(pld9xSet(), 16);
-		sl << QString("  Status            : ") + QString::number(status());
-	sl << QString("  East / West       : ") + (isEast()? "East" : "West");
-	for(int i = 1; i <= NPORT; ++i)
-		sl << QString("  Port[%1]           : %2")
-		      .arg(i).arg((tcpu(i) != NULL)? tcpu(i)->objectName() : "n/a");
+  sl << QString().sprintf("AnSerdes(%p):", this);
+  sl << QString("  Name              : ") + name();
+  sl << QString("  Hardware Address  : ") + haddr().toString().toStdString().c_str();
+  sl << QString("  Logical Address   : ") + laddr().toString().toStdString().c_str();
+  sl << QString("  Installed         : ") + (installed() ? "yes" : "no");
+  sl << QString("  Active            : ") + (active() ? "yes" : "no");
+  sl << QString("  Synchronized      : ") + synced().toString();
+  sl << QString("  Firmware ID       : ") + firmwareString();
+  sl << QString("  ECSR              : 0x") + QString::number(ecsr(), 16);
+  sl << QString("  PLD REG9x Set     : 0x") + QString::number(pld9xSet(), 16);
+  sl << QString("  Status            : ") + QString::number(status());
+  sl << QString("  East / West       : ") + (isEast()? "East" : "West");
+  for(int i = 1; i <= NPORT; ++i)
+    sl << QString("  Port[%1]           : %2")
+      .arg(i).arg((tcpu(i) != NULL)? tcpu(i)->objectName() : "n/a");
 
-	return sl.join("\n");
+  return sl.join("\n");
 }
 
 /**
@@ -113,22 +113,22 @@ void AnSerdes::sync(int level)
 QString AnSerdes::ecsrString() const
 {
   static const char* msg_list[] = {
-      "Channel 0 is On",
-      "Channel 1 is On",
-      "Channel 2 is On",
-      "Channel 3 is On",
-      "Test Data(0) / Real Data(1)",
-      "NOT USED",
-      "NOT USED",
-      "NOT USED", NULL };
+    "Channel 0 is On",
+    "Channel 1 is On",
+    "Channel 2 is On",
+    "Channel 3 is On",
+    "Test Data(0) / Real Data(1)",
+    "NOT USED",
+    "NOT USED",
+    "NOT USED", NULL };
 
   quint8 bts = ecsr();
   QString ret = "<h4>SERDES Register</h4>\n";
 
   ret += "<table>\n";
   for (int i = 0; i < 8; ++i)
-   ret += QString("<tr><td>[%1]</td><td>%2</td><td>= %3</td></tr>\n").
-         arg(i).arg(msg_list[i]).arg((bts >> i) & 0x1);
+    ret += QString("<tr><td>[%1]</td><td>%2</td><td>= %3</td></tr>\n").
+      arg(i).arg(msg_list[i]).arg((bts >> i) & 0x1);
   ret += "</table>";
 
   return ret;
@@ -138,72 +138,73 @@ QString AnSerdes::ecsrString() const
 //-----------------------------------------------------------------------------
 quint32 AnSerdes::canidr() const
 {
-	return haddr().at(1) << 4 | 0x4;
+  return haddr().at(1) << 4 | 0x4;
 }
 
 quint32 AnSerdes::canidw() const
 {
-	return haddr().at(1) << 4 | 0x2;
+  return haddr().at(1) << 4 | 0x2;
 }
 
 quint32 AnSerdes::cantyp() const
 {
-	return MSGTYPE_STANDARD;
+  return MSGTYPE_STANDARD;
 }
 
 AnAgent *AnSerdes::agent() const
 {
-	return dynamic_cast<AnRoot*>(parent()->parent())->agent(hAddress().at(0));
+  return dynamic_cast<AnRoot*>(parent()->parent())->agent(hAddress().at(0));
 }
 
 int AnSerdes::status() const
 {
-// TO-DO implement real logic
-	if (agent()->commError() || commError()) return STATUS_COMM_ERR;
+  // TO-DO implement real logic
+  if (agent()->commError() || commError()) return STATUS_COMM_ERR;
 
-	int err = 0;
-	quint8 pld9x = ecsr();
+  int err = 0;
+  quint8 pld9x = ecsr();
 
-// MTD patch
-//	if (laddr().at(1) == 4 && laddr().at(2) == 8) {
-//		if (m_pld9xBase) pld9x |= 0x4;
-//	}
+  // MTD patch
+  //	if (laddr().at(1) == 4 && laddr().at(2) == 8) {
+  //		if (m_pld9xBase) pld9x |= 0x4;
+  //	}
 
-	if (pld9x != pld9xSet()) err++;
+  if (pld9x != pld9xSet()) err++;
 
-	if (active()) {
-		if (err) return STATUS_ERROR;
-		return STATUS_ON;
-	} else
-		return STATUS_UNKNOWN;
+  if (active()) {
+    if (err) return STATUS_ERROR;
+    return STATUS_ON;
+  } else
+    return STATUS_UNKNOWN;
 }
 
 quint8 AnSerdes::pld9xSet() const {
-	quint8 bts = m_pld9xBase;
-	if (bts != 0) {
-		for (int i = 0; i < 4; ++i)
-			if (m_tcpu[i] && m_tcpu[i]->fibermode()) bts |= (1 << i);
-// MTD patch
-//		if (laddr().at(1) == 4 && laddr().at(2) == 8) {
-//			bts |= 0x4;
-//		}
-	}
-	return bts;
+  quint8 bts = m_pld9xBase;
+  if (bts != 0) {
+    for (int i = 0; i < 4; ++i)
+      if (m_tcpu[i] && m_tcpu[i]->fibermode()) bts |= (1 << i);
+    // MTD patch
+    //		if (laddr().at(1) == 4 && laddr().at(2) == 8) {
+    //			bts |= 0x4;
+    //		}
+  }
+  return bts;
 }
 
 QString AnSerdes::pld9xString(bool hilit) const
 {
-	QString ret = "0x" + QString::number(ecsr(), 16);
-	if (hilit && (status() == STATUS_ERROR))
-		ret = QString("<font color='red'>%1</font>").arg(ret);
-	ret += " (0x" + QString::number(pld9xSet(), 16) + ")";
-	return ret;
+  QString ret = "0x" + QString::number(ecsr(), 16);
+  if (hilit && (status() == STATUS_ERROR))
+    ret = QString("<font color='red'>%1</font>").arg(ret);
+  ret += " (0x" + QString::number(pld9xSet(), 16) + ")";
+  return ret;
 }
 
 //-----------------------------------------------------------------------------
 void AnSerdes::relink(int port)
 {
-  if (active() && port >= 1 && port <= 4 && commError() == 0) {
+  // allow for up to 3 communication errors, before giving up for good
+  if (active() && (port >= 1) && (port <= 4) && (commError() < 3)) {
     try {
       TPCANMsg    msg;
       TPCANRdMsg  rmsg;
@@ -214,6 +215,8 @@ void AnSerdes::relink(int port)
 		       0x90 + srdid, pld9xSetoff,
 		       0x90 + srdid, pld9xSet());
       agent()->write_read(msg, rmsg, 2);
+      // clear communication errors, since call succeeded
+      clearCommError();
     } catch (AnExCanError ex) {
       if (ex.status() == CAN_ERR_QRCVEMPTY) {
 	// probably harmless, just log
@@ -225,15 +228,15 @@ void AnSerdes::relink(int port)
       }
     }
   } else {
-    log(QString("relink problem: active=%1, port=%2, commError=%3")
+    log(QString("relink not issued: active=%1, port=%2, commError=%3")
 	.arg(active()).arg(port).arg(commError()));
   }
 }
 
 void AnSerdes::log(QString str) const
 {
-	foreach(QString s, str.split("\n")) {
-		agent()->root()->log(QString("AnSerdes[%1.%2]: " + s).arg(laddr().at(1)).arg(laddr().at(2)));
-	}
+  foreach(QString s, str.split("\n")) {
+    agent()->root()->log(QString("AnSerdes[%1.%2]: " + s).arg(laddr().at(1)).arg(laddr().at(2)));
+  }
 }
 
