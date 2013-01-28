b45fc8005ffe030704d6dd82d4392e6873bc6b03
J Schambach <jschamba@physics.utexas.edu>
2013-01-28 15:21:28 +0000

    can4 -> can5 for VPD-East

--- anaconda_two/db/devices.sql
+++ anaconda_two/db/devices.sql
@@ -11,7 +11,7 @@ insert into devices values (2, 2, "CANBus NE", 't');
 insert into devices values (3, 3, "CANBus SW", 't');
 insert into devices values (4, 6, "CANBus SE", 't');
 insert into devices values (5, 0, "CANBus VPD W", 't');
-insert into devices values (6, 4, "CANBus VPD E", 't');
+insert into devices values (6, 5, "CANBus VPD E", 't');
 -- insert into devices values (7, 5, "CANBus MTD1", 't');
 -- select * from devices;
 commit transaction;
