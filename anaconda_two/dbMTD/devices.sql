399a1aaca0146018ad6e2f073c8f9e124abec93b
J Schambach <jschamba@physics.utexas.edu>
2013-01-28 15:22:21 +0000

    can5 -> can4 for M-S

--- anaconda_two/dbMTD/devices.sql
+++ anaconda_two/dbMTD/devices.sql
@@ -6,7 +6,7 @@ create table devices (
 	installed	boolean  not null default 'f'
 );
 begin transaction;
-insert into devices values (1, 5, "CANBus M-S", 't');
+insert into devices values (1, 4, "CANBus M-S", 't');
 insert into devices values (2, 7, "CANBus M-N", 't');
 -- insert into devices values (7, 5, "CANBus MTD1", 't');
 -- select * from devices;
