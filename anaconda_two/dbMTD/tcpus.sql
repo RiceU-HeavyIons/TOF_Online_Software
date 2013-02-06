7fc53275dac125c1d020abf543df3d319d562ee5
J Schambach <jschamba@physics.utexas.edu>
2013-02-06 17:16:20 +0000

    disable BL10 correct LV boxes

--- anaconda_two/dbMTD/tcpus.sql
+++ anaconda_two/dbMTD/tcpus.sql
@@ -19,16 +19,16 @@ create table tcpus (
 
 begin transaction;
 -- North
-insert into tcpus values (  1, 2, 33, 147, 216, 2, 'A0',  4,  6,  1,  1,  1);
-insert into tcpus values (  2, 2, 34, 154, 219, 2, 'A1',  4,  7,  1,  1,  1);
-insert into tcpus values (  3, 2, 35, 185, 254, 2, 'A2',  4,  8,  1,  1,  1);
-insert into tcpus values (  4, 2, 36, 192, 268, 2, 'A3',  4,  9,  1,  1,  1);
-insert into tcpus values (  5, 2, 37, 188, 262, 2, 'B0',  4, 10,  1,  1,  1);
-insert into tcpus values (  6, 2, 38, 190, 260, 2, 'B1',  4, 11,  1,  1,  1);
-insert into tcpus values (  7, 2, 39, 174, 245, 2, 'B2', 12,  0,  1,  1,  1);
+insert into tcpus values (  1, 2, 33, 147, 216, 2, 'A0',  0,  6,  1,  1,  1);
+insert into tcpus values (  2, 2, 34, 154, 219, 2, 'A1',  0,  7,  1,  1,  1);
+insert into tcpus values (  3, 2, 35, 185, 254, 2, 'A2',  0,  8,  1,  1,  1);
+insert into tcpus values (  4, 2, 36, 192, 268, 2, 'A3',  0,  9,  1,  1,  1);
+insert into tcpus values (  5, 2, 37, 188, 262, 2, 'B0',  0, 10,  1,  1,  1);
+insert into tcpus values (  6, 2, 38, 190, 260, 2, 'B1',  0, 11,  1,  1,  1);
+insert into tcpus values (  7, 2, 39, 174, 245, 2, 'B2',  1,  0,  1,  1,  1);
 insert into tcpus values (  8, 2, 40,   0,   0, 2, 'B3', -1,  0,  1,  1,  0);
 insert into tcpus values (  9, 2, 41,   0,   0, 2, 'C0', -1,  0,  1,  1,  0);
-insert into tcpus values ( 10, 2, 42, 196, 264, 2, 'C1', -1,  0,  1,  1,  1);
+insert into tcpus values ( 10, 2, 42, 196, 264, 2, 'C1',  1,  1,  1,  1,  0);
 insert into tcpus values ( 11, 2, 43,   0,   0, 2, 'C2', -1,  0,  1,  1,  0);
 insert into tcpus values ( 12, 2, 44,   0,   0, 2, 'C3', -1,  0,  1,  1,  0);
 insert into tcpus values ( 13, 2, 45,   0,   0, 2, 'D0', -1,  0,  1,  1,  0);
@@ -42,14 +42,14 @@ insert into tcpus values ( 18, 1, 50,   0,   0, 1, 'A2', -1,  0,  1,  1,  0);
 insert into tcpus values ( 19, 1, 51,   0,   0, 1, 'A3', -1,  0,  1,  1,  0);
 insert into tcpus values ( 20, 1, 52,   0,   0, 1, 'B0', -1,  0,  1,  1,  0);
 insert into tcpus values ( 21, 1, 53,   0,   0, 1, 'B1', -1,  0,  1,  1,  0);
-insert into tcpus values ( 22, 1, 54, 181, 250, 1, 'B2', -1,  0,  1,  1,  1);
+insert into tcpus values ( 22, 1, 54, 181, 250, 1, 'B2',  1,  2,  1,  1,  1);
 insert into tcpus values ( 23, 1, 55,   0,   0, 1, 'B3', -1,  0,  1,  1,  0);
 insert into tcpus values ( 24, 1, 56,   0,   0, 1, 'C0', -1,  0,  1,  1,  0);
-insert into tcpus values ( 25, 1, 57, 156, 218, 1, 'C1',  4,  0,  1,  1,  1);
-insert into tcpus values ( 26, 1, 58, 140, 211, 1, 'C2',  4,  1,  1,  1,  1);
-insert into tcpus values ( 27, 1, 59, 135, 206, 1, 'C3',  4,  2,  1,  1,  1);
-insert into tcpus values ( 28, 1, 60, 131, 202, 1, 'D0',  4,  3,  1,  1,  1);
-insert into tcpus values ( 29, 1, 61, 146, 215, 1, 'D1',  4,  4,  1,  1,  1);
-insert into tcpus values ( 30, 1, 62, 148, 217, 1, 'D2',  4,  5,  1,  1,  1);
+insert into tcpus values ( 25, 1, 57, 156, 218, 1, 'C1',  0,  0,  1,  1,  1);
+insert into tcpus values ( 26, 1, 58, 140, 211, 1, 'C2',  0,  1,  1,  1,  1);
+insert into tcpus values ( 27, 1, 59, 135, 206, 1, 'C3',  0,  2,  1,  1,  1);
+insert into tcpus values ( 28, 1, 60, 131, 202, 1, 'D0',  0,  3,  1,  1,  1);
+insert into tcpus values ( 29, 1, 61, 146, 215, 1, 'D1',  0,  4,  1,  1,  1);
+insert into tcpus values ( 30, 1, 62, 148, 217, 1, 'D2',  0,  5,  1,  1,  1);
 
 commit transaction;
